596c1e4c3f5d0b3968d8f7b05efc29b9134910f5
Leonard-Calix <36576933+Leonard-Calix@users.noreply.github.com>
2018-02-22 10:51:42 -0600

    agrege las clases de col y row
    se modifico el contenido

--- Kickstarter/Plantillas/navegacion.php
+++ Kickstarter/Plantillas/navegacion.php
@@ -1,12 +1,12 @@
-<nav class="navegacion contenedor">
-      <ul class="enlaces">
-        <a class="primero"href="#">  Arte</a>
-        <a href="#">Cómics & ilustración</a>
-        <a href="#">Diseño & tecnología</a>
-        <a href="#">Cine</a>
-        <a href="#">Comida & creaciones</a>
-        <a href="#">Juegos</a>
-        <a href="#">Música</a>
-        <a href="#">Publicaciones</a>
+<nav class="navegacion container">
+      <ul class="venlaces">
+        <a class=" enlace-footer primero" href="#">  Arte</a>
+        <a class="enlace-footer" href="#">Cómics & ilustración</a>
+        <a class="enlace-footer" href="#">Diseño & tecnología</a>
+        <a class="enlace-footer" href="#">Cine</a>
+        <a class="enlace-footer" href="#">Comida & creaciones</a>
+        <a class="enlace-footer" href="#">Juegos</a>
+        <a class="enlace-footer" href="#">Música</a>
+        <a class="enlace-footer" href="#">Publicaciones</a>
       </ul>
   </nav>
--- Kickstarter/empieza-tu-proyecto.php
+++ Kickstarter/empieza-tu-proyecto.php
@@ -1,53 +1,262 @@
   <?php include 'Plantillas/header.php';   ?>
 
-  <div class=" titulo contenedor">
-    <h1>Convierte tu proyecto <br> creativo en realidad.</h1>
+  <div class=" acomodar espacio contenedor">
+    <div class="container">
+       <div class="row">
+         <div class="col">
+           <h1>Convierte tu proyecto <br> creativo en realidad.</h1>
+         </div>
+       </div>
+    </div>
   </div>
 
-  <form class="titulo contenedor" action="empieza-tu-proyecto1.php" method="post">
+  <form class="acomodar espacio contenedor" action="empieza-tu-proyecto1.php" method="post">
      <input class="btn btn-success inicio" type="submit" name="Empieza tu proyecto" value="Empieza tu proyecto">
   </form>
 
+    <div class="container imagenes">
+      <div class="row">
+        <div class="col">
+          <img src="img/img1.png" alt="">
+        </div>
+        <div class="col">
+          <img src="img/img2.png" alt="">
+          <img src="img/img3.png" alt="">
+        </div>
+      </div>
+    </div>
 
-  <div class="container fluid tabla-imagenes">
-    <table class="container fluid">
-      <td class="imagenes">
-        <img src="img/img1.png" alt="">
-      </td>
-      <td class="imagenes" >
-        <img src="img/img2.png" alt="">
-        <img src="img/img2.png" alt="">
-      </td>
-    </table>
-  </div>
+    <section class="anuncio">
+         <div class="container">
+            <div class="row">
+              <div class="col">
+                <h1 >
+                  We see Kickstarter as a home for creative minds <br> and a wonderful platform; where people who <br> believe,
+                  respect, and see the vision can support an <br> idea and make it a reality.<br><br>
+               </h1>
+               <h6>— DE LA SOUL</h6>
+              </div>
+            </div>
+         </div>
+    </section>
 
-   <section class="anuncio">
-     <h3 >
-       We see Kickstarter as a home for creative minds <br> and a wonderful platform; where people who <br> believe,
-       respect, and see the vision can support an <br> idea and make it a reality.<br><br>
-      — DE LA SOUL
-    </h3>
-   </section>
-
-   <div class="resumen contenedor">
+   <div class="resumen container">
       <h3>Un proyecto de Kickstarter hace mucho más <br> que simplemente recaudar dinero. <br> Genera una comunidad en torno a tu trabajo.</h3>
    </div>
 
-   <div class="contenedor tabla2">
-     <table>
-       <td>
-         <h4>¿Qué tipo de proyectos <br> puedo financiar <br> con Kickstarter?</h4>
-       </td>
-       <td> <h6>Kickstarter está diseñado específicamente para proyectos <br>
-         creativos en las siguientes categorías: Arte, Comics,<br>
-         Artesanías, Baile, Diseño, Moda, Cine y vídeo, Comida, <br>
-         Juegos, Periodismo, Música, Fotografía, Publicaciones, <br>
-         Tecnología, y Teatro. Ya sea que quieras publicar un álbum, <br>
-         escribir un libro, crear una obra de teatro o rodar una película <br>
-         — has llegado al sitio indicado. Para más información, <a href="#">lee reglas del proyecto</a> .</h6>
-       </td>
+   <div class="container espacio-contenido">
+     <div class=" row">
+       <div class=" col">
+           <h4>¿Qué tipo de proyectos <br> puedo financiar <br> con Kickstarter?</h4>
+       </div>
 
-      d
+       <div class="col">
+         <h6>Kickstarter está diseñado específicamente para proyectos <br>
+           creativos en las siguientes categorías: Arte, Comics,<br>
+           Artesanías, Baile, Diseño, Moda, Cine y vídeo, Comida, <br>
+           Juegos, Periodismo, Música, Fotografía, Publicaciones, <br>
+           Tecnología, y Teatro. Ya sea que quieras publicar un álbum, <br>
+           escribir un libro, crear una obra de teatro o rodar una película <br>
+           — has llegado al sitio indicado. Para más información, <a href="#">lee reglas del proyecto</a> .</h6>
+       </div>
 
-     </table>
+     </div>
    </div>
+
+   <div class="container espacio-contenido ">
+     <div class=" row">
+       <div class=" col">
+
+           <h4>¿Quién puede<br>contribuir <br>   a mi proyecto??</h4>
+       </div>
+
+       <div class="col">
+         <h6>Millones de personas visitan Kickstarter cada semana, pero el <br>
+         apoyo empieza siempre con la gente que conoces. Los amigos, <br>
+         los fans y las comunidades de las cuales formas parte <br>
+         no sólo serán tus primeros patrocinadores, sino también tu <br>
+         mayor recurso para correr la voz acerca de tu proyecto..</h6>
+       </div>
+
+     </div>
+   </div>
+
+
+   <div class="container espacio-contenido ">
+     <div class=" row">
+       <div class=" col">
+
+           <h4>¿Es mucho trabajo<br>llevar un proyecto? </h4>
+       </div>
+
+       <div class="col">
+         <h6> Cada proyecto de Kickstarter tiene momentos excitantes y <br>
+         desafiantes, pero la cantidad de trabajo, generalmente, <br>
+         depende del tamaño y de la complejidad del proyecto.<br><br>
+
+         <h6>Por lo general, los primeros días son los que más trabajo <br>
+         requieren - tienes que correr la voz, contestar preguntas de <br>
+         potenciales patrocinadores, etc. También en los últimos días <br>
+         puede ser que estés muy ocupado promoviendo tu proyecto <br>
+         en las redes sociales para alcanzar la meta.</h6><br>
+
+         <h6>En ocasiones, algunos proyectos cobran vida propia. Si éste <br>
+         es el caso, aprovecha el tiempo para concentrarte más en la creación y la <br>
+         entrega de las recompensas.</h6>
+       </div>
+
+     </div>
+   </div>
+
+   <div class="container espacio-contenido">
+     <div class="row">
+           <div class="col">
+             <h4>¿Cómo me comunico <br> si tengo preguntas?</h4>
+           </div>
+
+           <div class="col">
+             <h6>Si tienes preguntas, puedes ponerte en contacto con nosotros <br>
+             cuando gustes, usando este formulario de contacto. También <br>
+             te recomendamos que eches un vistazo a nuestras preguntas <br>
+             frecuentes, donde se explican varios temas con más detalle. <br>
+             En el manual del creador encontrarás consejos para elaborar y <br>
+             administrar un proyecto.</h6>
+           </div>
+     </div>
+   </div>
+
+    <div class="seccion-video">
+      <div class="contenedor">
+        <video  class="video" src="video/wed.mp4"  > </video>
+      </div>
+   </div>
+
+
+
+      <div class="container espacio-contenido">
+        <div class="row">
+          <div class="col">
+            <h4>1 <br>
+            Kickstarter es una <br>
+            plataforma <br>
+            exclusivamente para <br>
+            proyectos creativos.</h4>
+          </div>
+
+           <div class="col">
+             <h6>Creamos Kickstarter como una herramienta para artistas, <br>
+             diseñadores, makers, músicos y gente creativa en todo el <br>
+             mundo. Nos enorgullece ser la única plataforma dedicada <br>
+             exclusivamente a la comunidad creativa.</h6>
+            </div>
+          </div>
+      </div>
+
+
+      <div class="container espacio-contenido">
+        <div class="row">
+          <div class="col">
+            <h4>2 <br>
+            Todo o nada - un <br>
+            modelo de <br>
+            financiamiento que funciona.</h4>
+          </div>
+
+          <div class="col">
+            <h6>Nuestro modelo permite a los creadores establecer una meta <br>
+              de financiamiento y un margen de tiempo en el cual lo quieren <br>
+              conseguir. De esta manera no corren el riesgo de tener que <br>
+              llevar a cabo un proyecto sin haber conseguido todos los <br>
+              fondos necesarios. Al mismo tiempo, a los patrocinadores les <br>
+              sirve como incentivo para contribuir más para que se llegue <br>
+              a la meta.</h6>
+          </div>
+        </div>
+      </div>
+
+      <div class="container espacio-contenido">
+        <div class="row">
+          <div class="col">
+            <h4>3 <br>
+              Hay toda una <br>
+              comunidad que quiere <br>
+              apoyarte.</h4>
+          </div>
+
+          <div class="col">
+            <h6>Millions of backers agree — helping to create something new <br>
+              is exciting. People love peeking behind the creative curtain <br>
+              and directly supporting the creative process. In fact, 13.9 <br>
+              million people have pledged more than $3.39 billion to bring <br>
+              Kickstarter projects to life over the years.</h6>
+          </div>
+        </div>
+      </div>
+
+      <div class="container anuncio-crea">
+        <h1>Crea un proyecto en una de <br> las siguientes categorías</h1>
+      </div>
+
+     <?php include 'Plantillas/navegacion.php';   ?>
+
+     <div class="container espacio">
+       <div class="row ">
+           <div class="col">
+             <h6>Desde postales pintadas a mano hasta murales enormes - <br>
+                miles de proyectos de arte han abierto nuevos caminos e <br>
+                iniciado diálogos importantes, además de darle una una <br>
+                oportunidad a mucha gente de compartir su obra con el <br>
+                mundo.</h6>
+           </div>
+
+           <div class="col cuadro ">
+            <h5> <b>¿Te interesa?</b></h5>
+            <h6>Simplemente haz click en Inicio y comienza el <br>
+            diseño. Chécalo, y si te gusta, ¡compártelo <br>
+            con tus amigos!</h6><br>
+            <a href="#">Empieza Tu Proyecto</a><br>
+            <h6>¡Aquí estamos para ayudarte! Nuestro equipo de ayuda a la <br>
+            comunidad lo sabe todo sobre los proyectos en marcha en cada <br>
+            una de nuestras categorías. Déjanos un comentario sobre la idea <br>
+            de tu proyecto: art@kickstarter.com. ¿Tienes alguna otra <br>
+            pregunta? Visita nuestro Centro de ayuda.</h6>
+           </div>
+       </div>
+     </div>
+
+      <section class="container arte">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+
+            </div>
+
+            <div class="col">
+
+            </div>
+
+            <div class="col">
+
+            </div>
+
+            <div class="col">
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="anuncio-1 espacio ">
+        <div class="acomodar">
+          <div class="row">
+            <div class="col ">
+              <h1>“Una forma de que las personas <br>
+                 creativas controlen su destino.”</h1><br><br>
+              <h6>— Brian Fargo, creador de un proyecto exitoso en Kickstarter</h6> <br><br>
+              <a href="#">Empieza Tu Proyecto</a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <?php include 'Plantillas/footer.php';   ?>
