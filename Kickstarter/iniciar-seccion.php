19287ccf00495f49b482009c6e76306645b22f31
Leonard-Calix <36576933+Leonard-Calix@users.noreply.github.com>
2018-02-23 13:29:17 -0600

    modifique iniciar seccion
    modificacion

--- Kickstarter/iniciar-seccion.php
+++ Kickstarter/iniciar-seccion.php
@@ -6,17 +6,24 @@
     <form class="form-signin">
       <h1 class="h3 mb-3 font-weight-normal">Iniciar seccion</h1>
       <label for="inputEmail" class="sr-only form-control">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Correo Electronico" required autofocus>
       <label for="inputPassword " class="sr-only form-control">Password</label>
-      <input type="password" id="inputPassword espacio" class="form-control" placeholder="Password" required>
+      <input type="password" id="inputPassword espacio" class="form-control" placeholder="Contraseña" required>
+      <a class="derecha olvidaste" href="#">¿La Olvidaste?</a>
+      <a class="btn-inicio"  href="#"><button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar Seccion</button></a>
+
       <div class="checkbox mb-3 ">
+        <input class="checkbox mb-3 " type="checkbox" value="remember-me">
         <label>
             <font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> Recuérdame
-            <input style="vertical-align: inherit;" type="checkbox" value="remember-me">
         </label>
       </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+      O
+      <br>
+      <a class="" href="#"> <button class="btn-facebook btn btn-lg btn-primary btn-block" type="submit"><i class="fab fa-facebook-square"></i> Iniciar Seccion con facebook</button></a>
+      <br><br><br>
+      ¿Nuevo usuario en Kickstarter? <a href="#">¡Regístrame!</a>
+      <br><br>
 
 
     </form>
