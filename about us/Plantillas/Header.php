1f957cf4aa0889ec6c8828a92956392f7f07a3ce
Schierke1 <36467193+Schierke0@users.noreply.github.com>
2018-02-23 11:17:39 -0600

    pagina en proceso
    del footer about us

---
+++ about us/Plantillas/Header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Kickstarter</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+  </head>
+  <body>
+    <header class="header" >
+      <a class="derecha separar" href="#">Empieza Tu Proyecto</a>
+      <a class="logo" href="index.php"> <img src="img/logo.png" alt="Logo"></a>
+      <a class="izquierda" href="#"> Busqueda <i class="fas fa-search"></i> </a>
+      <a class="izquierda" href="registrarse.php">Registrarse</a>
+    </header>
