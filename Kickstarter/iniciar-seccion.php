bc6e495898ba72247d3f3ff0ba6b01e9b06a3dcd
Leonard-Calix <36576933+Leonard-Calix@users.noreply.github.com>
2018-02-23 14:16:16 -0600

    a
    a

--- Kickstarter/iniciar-seccion.php
+++ Kickstarter/iniciar-seccion.php
@@ -1,18 +1,18 @@
-  <?php include 'Plantillas/header.php';   ?>
+      <?php include 'Plantillas/header.php';   ?>
 
 
-      <div class="text-center">
+      <div class="text-center ">
 
-    <form class="form-signin">
+    <form class="form-signin text-center ">
       <h1 class="h3 mb-3 font-weight-normal">Iniciar seccion</h1>
       <label for="inputEmail" class="sr-only form-control">Email address</label>
       <input type="email" id="inputEmail" class="form-control" placeholder="Correo Electronico" required autofocus>
       <label for="inputPassword " class="sr-only form-control">Password</label>
       <input type="password" id="inputPassword espacio" class="form-control" placeholder="Contraseña" required>
       <a class="derecha olvidaste" href="#">¿La Olvidaste?</a>
-      <a class="btn-inicio"  href="#"><button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar Seccion</button></a>
+      <a  href="#"><button class="btn-inicio btn btn-lg btn-primary btn-block" type="submit">Iniciar Seccion</button></a>
 
-      <div class="checkbox mb-3 ">
+      <div class=" olvidaste checkbox mb-3 ">
         <input class="checkbox mb-3 " type="checkbox" value="remember-me">
         <label>
             <font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> Recuérdame
@@ -24,8 +24,6 @@
       <br><br><br>
       ¿Nuevo usuario en Kickstarter? <a href="#">¡Regístrame!</a>
       <br><br>
-
-
     </form>
 
 
