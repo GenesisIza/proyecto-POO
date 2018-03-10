3c9e482c03c50b6cdf344301c3a3d3782a4faeeb
Leonard-Calix <36576933+Leonard-Calix@users.noreply.github.com>
2018-03-10 13:43:13 -0600

    creando el index

--- Kickstarter/index.php
+++ Kickstarter/index.php
@@ -1,173 +1,142 @@
 
     <?php include 'Plantillas/header.php';   ?>
-  <div class"clearfix"> </div>
 
+    <div class"clearfix"> </div>
     <div class="contenedor">
         <div class="row">
-
-          <div class="col-md-3">
-             <h5>15 de febrero de 2018 <br> <b>Haciendo realidad proyectos creativos</b></h5>
+           <div class="col-md-3">
+             <h6>15 de febrero de 2018 <br> <b>Haciendo realidad proyectos creativos</b></h6>
           </div>
-
           <div class="col-md-3">
-              <h5>TOTAL DE PATROCINADORES <br> <b>100000.</b>  </h5>
+              <h6>TOTAL DE PATROCINADORES <br> <b>100000.</b>  </h6>
           </div>
-
           <div class="col-md-3">
-              <h5> FINANCIADOS <br> <b>200000.</b>  </h5>
+              <h6> FINANCIADOS <br> <b>200000.</b>  </h6>
           </div>
-
           <div class="col-md-3">
-             <h5>PROYECTOS ACTIVOS <br> <b>3558.</b> </h5>
+             <h6>PROYECTOS ACTIVOS <br> <b>3558.</b> </h6>
          </div>
-
         </div>
-
+    </div>
+    <br><br>
+    <div class="contenedor">
+           <ul class="nav">
+             <li class="nav-item">
+               <a class="nav-link " href="#">Publicaciones</a>
+             </li>
+             <li class="nav-item">
+                 <a class="nav-link active" href="#">Cine</a>
+             </li> 
+             <li class="nav-item">
+                <a class="nav-link" href="#">Juegos</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link " href="#">Cómics & ilustración</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link " href="#">Comida & creaciones</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link " href="#">Música</a>
+             </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Diseño & tecnología</a>
+             </li>    
+          </ul>
+          <hr style="border: 0.4px solid gray;">
     </div>
 
-    <nav class="navegacion contenedor">
-          <ul class="enlaces">
-            <a class="primero"href="#">Cine</a>
-            <a href="#">Diseño & tecnología</a>
-            <a href="#">Adiv
-            <a href="#">Juegos</a>
-            <a href="#">Cómics & ilustración</a>
-            <a href="#">Comida & creaciones</a>
-            <a href="#">Música</a>
-            <a href="#">Publicaciones</a>
-          </ul>
-      </nav>
+    <div class="contenedor">
+            <div class="row">
+                <div class="col-md-6 img-fluid ">
+                    <img src="img/arte.jpg" alt="foto1">
+                </div>
 
-   <section class="contenedor">
-     <div class="row">
-       <div class="col-md-6 img-fluid ">
-          <img src="img/arte.jpg" alt="foto1">
-       </div>
-       <div class="col-md-6">
-         <nav class="pull-rigth nav">
-           <a class="nav-link active" href="#">NUEVOS Y DESCTACADOS</a>
-           <a class="nav-link" href="#">POPULAR</a>
-         </nav><br>
-         <table class="table">
-             <tbody>
-               <tr>
-                   <td><a href="#"><img src="img/arte1.png" alt=""></a></td>
-                   <td>@mdo</td>
-               </tr>
-               <tr>
-                 <td>  <a href="#"><img src="img/arte2.jpg" alt=""></a></td>
-                 <td>Thornton</td>
-                 <td>@fat</td>
-               </tr>
-               <tr>
-                 <th scope="row">3</th>
-                 <td>Larry</td>
-                 <td>the Bird</td>
-                 <td>@twitter</td>
-               </tr>
-             </tbody>
-         </table>
-       </div>
-     </div>
-   </section >
-
-
-<br><br>
-
-
-
-   <section class="container">
-     <div class="row">
-       <div class="col-md-6">
-          <img src="img/arte.jpg" alt="foto1">
-       </div>
-       <div class="col-md-6">
-         <nav class="pull-rigth nav">
-           <a class="nav-link active" href="#">Activea</a>
-           <a class="nav-link" href="#">Link</a>
-           <a class="nav-link" href="#">Link</a>
-           <a class="nav-link disabled" href="#">Disabled</a>
-         </nav><br>
-         <table class="table">
-           <caption>List of users</caption>
-             <tbody>
-               <tr>
-                 <th scope="row">1</th>
-                 <td>Mark</td>
-                 <td>Otto</td>
-                 <td>@mdo</td>
-               </tr>
-               <tr>
-                 <th scope="row">2</th>
-                 <td>Jacob</td>
-                 <td>Thornton</td>
-                 <td>@fat</td>
-               </tr>
-               <tr>
-                 <th scope="row">3</th>
-                 <td>Larry</td>
-                 <td>the Bird</td>
-                 <td>@twitter</td>
-               </tr>
-             </tbody>
-         </table>
-       </div>
-     </div>
-   </section >
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   <h6>Te Recomendamos</h6><br>
-
-     <section class="contenedor contenido row">
-
-          <div class="col-md-3">
-            1
+                <table class="table col-md-6">
+                    <tbody>
+                      <tr>
+                         <th scope="row"></th>
+                         <td> <a href=""> <img src="img/arte1.png"> </a> </td>
+                         
+                      </tr>
+                      <tr>
+                         <th scope="row"></th>
+                         <td> <a href=""> <img src="img/arte2.jpg" > </a> </td>
+                      </tr>
+                  </tbody>
+                </table>
           </div>
-          <div class="col-md-3">
-           2
-          </div>
-          <div class="col-md-3">
-           3
-          </div>
-          <div class="col-md-3">
-           4
+    </div>
+    <br><br><br>
+    <hr style="border: 0.4px solid gray;">
+    <br><br>
+    <div class="contenedor">
+         <div class="row img-fluid">
+              <div class="col-md-3">
+                <img src="img/a.jpg">
+              </div>
+              <div class="col-md-3">
+                <img src="img/b.png">
+              </div>
+              <div class="col-md-3">
+                <img src="img/c.jpg">
+              </div>
+              <div class="col-md-3">
+                <img src="img/d.png">
+              </div>
          </div>
+    </div>
+    <br><br>
 
-   </section>
+    <div class="contenedor">
+        <h1>Lo unicos que puedes hacer es hacer todo lo <br> que puedas</h1>
+    </div>
+
+    <div class="contenedor">
+          <div class="row">
+              <div class="col-md-6 img-fluid">
+                <img src="img/A.png">
+              </div>
+              <div class="col-md-6 img-fluid">
+                <img src="img/A.png ">
+              </div>
+          </div>
+    </div>
 
 
 
-     <?php include 'Plantillas/navegacion.php';   ?>
+     <hr style="border: 0.4px solid gray;">
+     <br><br>           
+     <div class="contenedor">
+           <ul class="nav">
+             <li class="nav-item">
+               <a class="nav-link " href="#">Arte</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link " href="#">Cómics & ilustración</a>
+             </li>
+             <li class="nav-item">
+                <a class="nav-link" href="#">Diseño & tecnología</a>
+             </li>    
+             <li class="nav-item">
+                 <a class="nav-link active" href="#">Cine</a>
+             </li> 
+             <li class="nav-item">
+               <a class="nav-link " href="#">Comida & creaciones</a>
+             </li>
+             <li class="nav-item">
+                <a class="nav-link" href="#">Juegos</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link " href="#">Música</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link " href="#">Publicaciones</a>
+             </li>
+          </ul>
+          <hr style="border: 0.4px solid gray;">
+    </div>
+
      <?php include 'Plantillas/footer.php';   ?>
   </body>
 </html>
