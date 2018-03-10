675ab667823df7e2da07c54db0426fc5b25a2602
Leonard-Calix <36576933+Leonard-Calix@users.noreply.github.com>
2018-03-10 11:10:56 -0600

    modifique el index

--- Kickstarter/Plantillas/Header.php
+++ Kickstarter/Plantillas/Header.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
     <title>Kickstarter</title>
     <link rel="stylesheet" href="css/fontawesome-all.min.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
--- Kickstarter/index.php
+++ Kickstarter/index.php
@@ -1,22 +1,35 @@
 
     <?php include 'Plantillas/header.php';   ?>
+  <div class"clearfix"> </div>
 
-    <section class="contenedor tabla">
-         <table>
-           <td >
-             <td>15 de febrero de 2018 <br> <b>Haciendo realidad proyectos creativos</b></th>
-             <td>TOTAL DE PATROCINADORES <br> <b>100000.</b>  </th>
-             <td>PROYECTOS FINANCIADOS <br> <b>200000.</b>  </th>
-             <td>PROYECTOS ACTIVOS <br> <b>3558.</b> </th>
-           </td>
-         </table>
-    </section>
+    <div class="contenedor">
+        <div class="row">
+
+          <div class="col-md-3">
+             <h5>15 de febrero de 2018 <br> <b>Haciendo realidad proyectos creativos</b></h5>
+          </div>
+
+          <div class="col-md-3">
+              <h5>TOTAL DE PATROCINADORES <br> <b>100000.</b>  </h5>
+          </div>
+
+          <div class="col-md-3">
+              <h5> FINANCIADOS <br> <b>200000.</b>  </h5>
+          </div>
+
+          <div class="col-md-3">
+             <h5>PROYECTOS ACTIVOS <br> <b>3558.</b> </h5>
+         </div>
+
+        </div>
+
+    </div>
 
     <nav class="navegacion contenedor">
           <ul class="enlaces">
             <a class="primero"href="#">Cine</a>
             <a href="#">Diseño & tecnología</a>
-            <a href="#">Arte</a>
+            <a href="#">Adiv
             <a href="#">Juegos</a>
             <a href="#">Cómics & ilustración</a>
             <a href="#">Comida & creaciones</a>
@@ -25,13 +38,134 @@
           </ul>
       </nav>
 
-   <section class="contenedor contenido">
-     <img src="img/foto1.png" alt="foto1"
+   <section class="contenedor">
+     <div class="row">
+       <div class="col-md-6 img-fluid ">
+          <img src="img/arte.jpg" alt="foto1">
+       </div>
+       <div class="col-md-6">
+         <nav class="pull-rigth nav">
+           <a class="nav-link active" href="#">NUEVOS Y DESCTACADOS</a>
+           <a class="nav-link" href="#">POPULAR</a>
+         </nav><br>
+         <table class="table">
+             <tbody>
+               <tr>
+                   <td><a href="#"><img src="img/arte1.png" alt=""></a></td>
+                   <td>@mdo</td>
+               </tr>
+               <tr>
+                 <td>  <a href="#"><img src="img/arte2.jpg" alt=""></a></td>
+                 <td>Thornton</td>
+                 <td>@fat</td>
+               </tr>
+               <tr>
+                 <th scope="row">3</th>
+                 <td>Larry</td>
+                 <td>the Bird</td>
+                 <td>@twitter</td>
+               </tr>
+             </tbody>
+         </table>
+       </div>
+     </div>
+   </section >
+
+
+<br><br>
+
+
+
+   <section class="container">
+     <div class="row">
+       <div class="col-md-6">
+          <img src="img/arte.jpg" alt="foto1">
+       </div>
+       <div class="col-md-6">
+         <nav class="pull-rigth nav">
+           <a class="nav-link active" href="#">Activea</a>
+           <a class="nav-link" href="#">Link</a>
+           <a class="nav-link" href="#">Link</a>
+           <a class="nav-link disabled" href="#">Disabled</a>
+         </nav><br>
+         <table class="table">
+           <caption>List of users</caption>
+             <tbody>
+               <tr>
+                 <th scope="row">1</th>
+                 <td>Mark</td>
+                 <td>Otto</td>
+                 <td>@mdo</td>
+               </tr>
+               <tr>
+                 <th scope="row">2</th>
+                 <td>Jacob</td>
+                 <td>Thornton</td>
+                 <td>@fat</td>
+               </tr>
+               <tr>
+                 <th scope="row">3</th>
+                 <td>Larry</td>
+                 <td>the Bird</td>
+                 <td>@twitter</td>
+               </tr>
+             </tbody>
+         </table>
+       </div>
+     </div>
+   </section >
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <h6>Te Recomendamos</h6><br>
+
+     <section class="contenedor contenido row">
+
+          <div class="col-md-3">
+            1
+          </div>
+          <div class="col-md-3">
+           2
+          </div>
+          <div class="col-md-3">
+           3
+          </div>
+          <div class="col-md-3">
+           4
+         </div>
+
    </section>
 
-   <h1>Probando Git</h1>
 
-    <h1>Probando para sincronizar</h1>
 
      <?php include 'Plantillas/navegacion.php';   ?>
      <?php include 'Plantillas/footer.php';   ?>
