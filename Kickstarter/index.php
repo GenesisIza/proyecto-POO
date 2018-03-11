5ae886deff8eced8d8dfa8a0082afb584f20f831
Leonard-Calix <36576933+Leonard-Calix@users.noreply.github.com>
2018-03-11 12:19:06 -0600

    terminando el index

--- Kickstarter/index.php
+++ Kickstarter/index.php
@@ -2,6 +2,7 @@
     <?php include 'Plantillas/header.php';   ?>
 
     <div class"clearfix"> </div>
+    <br>
     <div class="contenedor">
         <div class="row">
            <div class="col-md-3">
@@ -18,15 +19,16 @@
          </div>
         </div>
     </div>
-    <br><br>
+    <hr style="border: 0.4px solid black;">
+    <br>
     <div class="contenedor">
            <ul class="nav">
              <li class="nav-item">
-               <a class="nav-link " href="#">Publicaciones</a>
-             </li>
-             <li class="nav-item">
                  <a class="nav-link active" href="#">Cine</a>
              </li> 
+             <li class="nav-item">
+               <a class="nav-link " href="#">Publicaciones</a>
+             </li>
              <li class="nav-item">
                 <a class="nav-link" href="#">Juegos</a>
              </li>
@@ -46,67 +48,161 @@
           <hr style="border: 0.4px solid gray;">
     </div>
 
-    <div class="contenedor">
+    <div style="border-bottom: 0.8px solid:black;" class="contenedor">
+             <h3>Cine</h3><br>
+             <h4>Proyecto Destacado</h4>
             <div class="row">
-                <div class="col-md-6 img-fluid ">
-                    <img src="img/arte.jpg" alt="foto1">
+                <div class="col-md-6  ">
+                    <img class="imagendetabla" src="img/img01.jpg" alt="foto1">
                 </div>
-
+              <div class="col-md-6">
+                <ul class="nav">
+                       <li class="nav-item">
+                           <a class="nav-link " href="#">Nuevos Y destacados</a>
+                       </li>
+                       <li class="nav-item">
+                           <a class="nav-link" href="#">Cerca de la Meta</a>
+                       </li>
+                       <li class="nav-item">
+                           <a class="nav-link" href="#">Popular</a>
+                       </li>                      
+                   </ul><br>
                 <table class="table col-md-6">
                     <tbody>
-                      <tr>
-                         <th scope="row"></th>
-                         <td> <a href=""> <img src="img/arte1.png"> </a> </td>
-                         
+                      <tr >
+                       <td><a href=""> <img src="img/cat-01.png"> </a>  </td>    
+                      </tr >
+                      <tr>   
+                         <td><a href=""> <img src="img/cat-02.jpg" > </a></td>
                       </tr>
-                      <tr>
-                         <th scope="row"></th>
-                         <td> <a href=""> <img src="img/arte2.jpg" > </a> </td>
+                      <tr >
+                       <td><a href=""> <img src="img/cat-03.png"> </a>  </td>    
+                      </tr >
+                      <tr>   
+                         <td><a href=""> <img src="img/cat-04.jpg" > </a></td>
                       </tr>
                   </tbody>
                 </table>
+                <button type="button" class="btn btn-light">Ver mas</button>
+            </div>
           </div>
+
     </div>
     <br><br><br>
-    <hr style="border: 0.4px solid gray;">
+    
     <br><br>
     <div class="contenedor">
          <div class="row img-fluid">
-              <div class="col-md-3">
-                <img src="img/a.jpg">
+              <div class="col-md-3">                             
+               <a href="">
+                    <img src="img/cat-05.jpg">
+                    <p>A new album of songs addressing mental <br>
+                    health issues, addiction, and family <br>
+                    dysfunction. Thank you for your support! <br>
+                    #SongsHeal <br><br>
+                    75 % financiado
+                    </p>
+               </a>
               </div>
               <div class="col-md-3">
-                <img src="img/b.png">
+                <a href="">
+                    <img src="img/cat-06.png">
+                    <p>
+                      13 artists blend the <br>
+                      fantastical with the scientific in a 60+ page collection of <br>
+                      fictitious critters inspired by 90s <br>
+                      era publications. <br><br>
+
+                     78 % financiado
+                    </p>
+                </a>     
               </div>
               <div class="col-md-3">
-                <img src="img/c.jpg">
+                <a href="">
+                  <img src="img/cat-07.jpg">
+                  <p>
+                    A children's STEM Talking Book series that <br>
+                     fuels curiosity and encourages children to <br>
+                      think like innovators. <br><br>
+                     79 % financiado
+                  </p>
+                </a>
+                
               </div>
               <div class="col-md-3">
-                <img src="img/d.png">
+                   <a href="">
+                     <img src="img/cat-08.jpg">
+                     <p>           
+                       A sorcerer saves a girl from flesh-eating <br>
+                       demons. After trying to use magic to find <br>
+                        her friends, he’s trapped in a vision of the past. <br><br>
+                        81 % financiado
+                     </p>
+                   </a>            
               </div>
          </div>
     </div>
+    
+    <hr style="border: 0.4px solid black;">
     <br><br>
-
-    <div class="contenedor">
-        <h1>Lo unicos que puedes hacer es hacer todo lo <br> que puedas</h1>
+    <hr style="border: 0.4px solid black;">
+    <br>
+  
+    <div class="contenedor align-baseline">
+        <img style="font-size: 1.2em;" src="img/logo2.png">
+        <h1 style="text-align: center;display: inline-flex;padding-left: 60px;">Lo unicos que puedes hacer es hacer todo lo <br> que puedas</h1>
     </div>
+    <br><br>
+    <hr style="border: 0.4px solid black;">
+    <br><br>
 
     <div class="contenedor">
           <div class="row">
               <div class="col-md-6 img-fluid">
-                <img src="img/A.png">
+                <img src="img/A.jpeg">
               </div>
               <div class="col-md-6 img-fluid">
-                <img src="img/A.png ">
+                <img src="img/drip.png ">
               </div>
           </div>
+    </div><br><br>
+     <hr style="border: 0.4px solid gray;">
+     <br>      
+
+     <div class="contenedor">
+        <h5>Lo que estamos leyendo</h5>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card" >
+                    <img class="card-img-top" src="img/h1.png" alt="Card image cap">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+               </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card" >
+                   <img class="card-img-top" src="img/h2.png" alt="Card image cap">
+                   <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                   </div>
+                </div>
+            </div>
+        
+            <div class="col-md-4">
+                 <div class="card" >
+                     <img class="card-img-top" src="img/h3.png" alt="Card image cap">
+                       <div class="card-body">
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                       </div>
+                 </div>   
+           </div>
+        </div>
     </div>
 
+  
 
-
-     <hr style="border: 0.4px solid gray;">
-     <br><br>           
      <div class="contenedor">
            <ul class="nav">
              <li class="nav-item">
@@ -134,8 +230,16 @@
                <a class="nav-link " href="#">Publicaciones</a>
              </li>
           </ul>
-          <hr style="border: 0.4px solid gray;">
+          
     </div>
+    
+
+
+
+
+
+
+
 
      <?php include 'Plantillas/footer.php';   ?>
   </body>
