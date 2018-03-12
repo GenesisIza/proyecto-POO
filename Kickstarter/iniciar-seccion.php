149e7d83b57df652666a22cfed33ff95e604070f
Schierke1 <36467193+Schierke0@users.noreply.github.com>
2018-03-11 18:00:05 -0600

    vuelva
    actualice de nuevo habia codigo repetido

--- Kickstarter/iniciar-seccion.php
+++ Kickstarter/iniciar-seccion.php
@@ -4,8 +4,6 @@
      <div class="formulario-reguistro" >
       
   <table align="center" cellpadding="15"  class="table-iniciar">
-
-<<<<<<< HEAD
         <td><h2 class="text-inicio">Iniciar sesión</h2></td>
     </tr>
     <tr>
@@ -37,32 +35,3 @@
 </table>
 </div>
     
-=======
-      <div class="text-center ">
-
-    <form class="form-signin text-center ">
-      <h1 class="h3 mb-3 font-weight-normal">Iniciar seccion</h1>
-      <label for="inputEmail" class="sr-only form-control">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Correo Electronico" required autofocus>
-      <label for="inputPassword " class="sr-only form-control">Password</label>
-      <input type="password" id="inputPassword espacio" class="form-control" placeholder="Contraseña" required>
-      <a class="derecha olvidaste" href="#">¿La Olvidaste?</a>
-      <a  href="#"><button class="btn-inicio btn btn-lg btn-primary btn-block" type="submit">Iniciar Seccion</button></a>
-
-      <div class=" olvidaste checkbox mb-3 ">
-        <input class="checkbox mb-3 " type="checkbox" value="remember-me">
-        <label>
-            <font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> Recuérdame
-        </label>
-      </div>
-      O
-      <br>
-      <a class="" href="#"> <button class="btn-facebook btn btn-lg btn-primary btn-block" type="submit"><i class="fab fa-facebook-square"></i> Iniciar Seccion con facebook</button></a>
-      <br><br><br>
-      ¿Nuevo usuario en Kickstarter? <a href="registrarse.php">¡Regístrame!</a>
-      <br><br>
-    </form>
-
-
-      </div>
->>>>>>> 742de6e0f33bfdb76d5b2fde197ec52fba8126e8
