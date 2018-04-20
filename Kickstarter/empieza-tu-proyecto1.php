577a2a5b65614d36820f01a4e1ccdebe48863729
Schierke1 <36467193+Schierke0@users.noreply.github.com>
2018-04-19 21:45:10 -0600

    ef

--- Kickstarter/empieza-tu-proyecto1.php
+++ Kickstarter/empieza-tu-proyecto1.php
@@ -14,18 +14,18 @@
     <link rel="stylesheet" type="text/css" href="css/estilos.css">
     <link rel="stylesheet" href="css/signin.css">
   </head>
-  <body>
+  <body onunload="validar();">
     <?php include 'Plantillas/header.php';   ?>
     <div class="formulario espacio ">
          <h3 id="primero">Primero hay que definir tu proyecto</h3>
          <h4 id="segundo">Escoge una categoría para que puedas llegar a la audiencia deseada.</h4>
          <br><br>
          <form class="" action="empieza-tu-proyecto2.php" method="post">
-            <select class="forma form-control form-control-lg">
+            <select class="forma form-control form-control-lg" id="slc-categoria">
               <option value="0">Selecione su categoria</option>
               <option value="1">Arte</option>
               <option value="2">Artesanias</option>
-              <option value="3">Cine Y videos</option>
+              <option value="3">Cine y videos</option>
               <option value="4">Comisc</option>
               <option value="5">Comida</option>
               <option value="6">Diseño</option>
@@ -39,9 +39,12 @@
            </select>
            <br><br>
            <hr>
-           <input class="btn btn-dark btn1" type="submit" name="" value="Siguiente:Idea para tu proyecto">
+           <input class="btn btn-dark btn1" type="submit" name="" value="Siguiente:Idea para tu proyecto"  id="btn-idea">
        </form>
   </div>
+   <script src="js/jquery-3.3.1.min.js"></script>  
+  <script src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/controlador.js"></script>
   </body>
   </html>
   
--- Kickstarter/empieza-tu-proyecto2.php
+++ Kickstarter/empieza-tu-proyecto2.php
@@ -20,10 +20,10 @@
         <h4 id="segundo">No te preocupes - puedes modificarlo más adelante.</h4>
         <br><br>
         <br><br>
-        <input class="texa" type="texa" name="" value="" placeholder="Un juego de targetas, papel, sobras hechos a mano"><br><br>
+        <input class="texa form-control " type="texarea" name="" value="" placeholder="Un juego de targetas, papel, sobras hechos a mano" onkeydown="validarProyecto(this);"><br><br>
         <hr>
         <a href="empieza-tu-proyecto1.php" class="empieza"> ← Categoria</a>
-        <a href="empieza-tu-proyect3.php"> <button type="button" class="btn btn-dark btn1">Siguiente: Ubicacion</button></a>
+        <a href="empieza-tu-proyect3.php"> <button type="button" class="btn btn-dark btn1" disabled="disabled">Siguiente: Ubicacion</button></a>
      </div>   
   </body>
   </html>
