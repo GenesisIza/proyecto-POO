ba3a46ee334fc2288fa8b935f744b9aa3563088e
Schierke1 <36467193+Schierke0@users.noreply.github.com>
2018-04-22 16:44:33 -0600

    style....

--- Kickstarter/empieza-tu-proyect3.php
+++ Kickstarter/empieza-tu-proyect3.php
@@ -6,7 +6,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="icon" type="image/png" href="img/logo3.png">
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">  
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet"> 
     <title>Kickstarter</title>
     <link rel="stylesheet" href="css/fontawesome-all.min.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
