df1f466a61682d2a9bea0481a3b6dfc72e564800
Schierke1 <36467193+Schierke0@users.noreply.github.com>
2018-04-22 19:01:55 -0600

    .

--- Kickstarter/Plantillas/Header.php
+++ Kickstarter/Plantillas/Header.php
@@ -1,10 +1,10 @@
 
     <header class="header">
-      <a class="derecha" href="#">Explorar</a>
-      <a class="derecha separar" href="empieza-tu-proyecto.php">Empieza Tu Proyecto</a>
+      <a class="derecha esconder" href="#">Explorar</a>
+      <a class="derecha separar esconder" href="empieza-tu-proyecto.php">Empieza Tu Proyecto</a>
       <a class="logo" href="index.php"> <img src="img/logo.png" alt="Logo" ></a>
-      <a class="izquierda" href="Iniciar-seccion.php">Registrarse</a>
-      <a class="izquierda" href="#"><i class="fas fa-search"></i></a>
-      <a class="izquierda Busqueda" href="#"> Busqueda  </a>
+      <a class="izquierda esconder" href="Iniciar-seccion.php">Registrarse</a>
+      <a class="izquierda esconder" href="#"><i class="fas fa-search"></i></a>
+      <a class="izquierda Busqueda esconder" href="#"> Busqueda  </a>
     </header>
     <br>
