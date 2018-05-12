fc0ed85195f99f168e47e928ce5287eb7f61c714
Leonard-Calix <36576933+Leonard-Calix@users.noreply.github.com>
2018-05-11 22:56:26 -0600

    header

--- Kickstarter/Plantillas/Header.php
+++ Kickstarter/Plantillas/Header.php
@@ -1,11 +1,22 @@
 
     <header class="header">
     	<link rel="stylesheet" type="text/css" href="css/estilos.css">
-      <a class="derecha esconder" href="#">Explorar</a>
-      <a class="derecha separar esconder" href="empieza-tu-proyecto.php">Empieza Tu Proyecto</a>
-      <a class="logo" href="index.php"> <img src="img/logo.png" alt="Logo" ></a>
-      <a class="izquierda esconder" href="Iniciar-seccion.php">Registrarse</a>
-      <a class="izquierda esconder" href="#"><i class="fas fa-search"></i></a>
-      <a class="izquierda Busqueda esconder" href="#"> Busqueda  </a>
+      <div class="row">
+        <div class="col-lg-4 col-lg-4 col-md-4">
+          <a class="esconder" href="#">Explorar</a>
+          <a class="esconder" href="empieza-tu-proyecto.php">Empieza Tu Proyecto</a>
+        </div>
+        <div class="col-lg-4 col-lg-4 col-md-4">
+          <a  class="logo top-center" href="index.php"> <img src="img/logo.png" alt="Logo" ></a>
+        </div>
+        <div class="col-lg-4 col-lg-4 col-md-4">
+          <a style=" float: right;" class=" esconder" href="Iniciar-seccion.php">Registrarse</a>
+          <a style=" float: right;" class=" esconder" href="#"><i class="fas fa-search"></i></a>
+          <a style=" float: right;" class=" Busqueda esconder" href="#"> Busqueda </a>
+        </div>
+      </div>
+
+
+
     </header>
     <br>
